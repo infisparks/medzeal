c72838313e14a6140d4b7c0dd5d7af46c6d94dc4
InfiSpark <infisparks@gmail.com>
2025-02-17 12:24:55 +0530

    Update page.js

--- src/app/admin/staff/page.js
+++ src/app/admin/staff/page.js
@@ -62,7 +62,7 @@ const AdminDashboard = () => {
           { title: "Mark User Present", icon: faUserCheck, link: "/admin/attend", color: "warning" },
           { title: "Today Appointments", icon: faCalendarDay, link: "/admin/todayattend", color: "info" },
           { title: "User History", icon: faHistory, link: "/admin/userhistory", color: "dark" },
-          { title: "Sell Product", icon: faBoxOpen, link: "/admin/inventrysell", color: "danger" },
+      //    { title: "Sell Product", icon: faBoxOpen, link: "/admin/inventrysell", color: "danger" },
           { title: "Download Invoice", icon: faFileInvoice, link: "/admin/invoice", color: "secondary" },
           { title: "Add Vendor", icon: faStore, link: "/admin/vendorentry", color: "dark" },
           { title: "All Vendors & Add Products", icon: faUsers, link: "/admin/vendors", color: "success" },
