bb65365aa817c48b38db972d44be70e0d76b6332
MK MUZAKKIR <muzakkir7977@gmail.com>
2025-02-21 15:59:07 -0500

    sdf

--- src/components/EmotionalMessagePopup.js
+++ src/components/EmotionalMessagePopup.js
@@ -93,6 +93,8 @@ export default function EmotionalMessagePopup() {
       <button style={buttonStyle} onClick={handleBookAppointment}>
         Book Appointment
       </button>
+
+    
     </div>
   );
 }
